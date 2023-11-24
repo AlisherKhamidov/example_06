2fdd73eaf96052433006dbac810478d26d541573
Osmanov Yakub <osmanov3010@gmail.com>
2023-11-24 14:00:17 +0100

    added method calcSum

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
-  public static void main(String[] args) {
-    int x = 10;
-    int y = 15;
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
 
-  }
+        int z = calcSum(x, y);
+        System.out.println(z);
+    }
+
+    private static int calcSum(int x, int y) {
+        return x + y;
+    }
 }
