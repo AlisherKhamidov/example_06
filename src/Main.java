cfb989ae17629dd5d6a7d102dfec3a3da66295ab
Igor Volotovskyi <ihor737@gmail.com>
2023-11-24 18:26:09 +0100

    add method sumOfNumbers

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@ public class Main {
     int x = 10;
     int y = 15;
 
+    int sum = sumOfNumbers(x, y);
+    System.out.println(x + " + " + y + " = " + sum);
+
+  }
+
+  private static int sumOfNumbers(int a, int b) {
+    int sum = a + b;
+    return sum;
   }
 }
