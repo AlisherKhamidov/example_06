f5ff416be452f881ed2b286673ba1b14e01df499
Vitaliy Kornienko <vit_ko@ukr.net>
2023-11-24 19:04:01 +0100

    add method sum to class main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
-  public static void main(String[] args) {
-    int x = 10;
-    int y = 15;
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        sum(x, y);
+    }
 
-  }
+    private static void sum(int x, int y) {
+        System.out.println("The sum of " + x + " and " + y + " = " + (x + y));
+    }
 }
