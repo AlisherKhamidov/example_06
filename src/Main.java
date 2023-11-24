ae690694836748deb23156cbbc4fb1eeb8038e64
Igor Kremenskoy <gores.73@web.de>
2023-11-24 16:29:44 +0100

    add and print sum

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
   public static void main(String[] args) {
     int x = 10;
     int y = 15;
-
+    int sum = addNumbers(x, y);
+    System.out.println("Сумма " + x + " и " + y + " равна: " + sum);
+  }
+  public static int addNumbers(int a, int b) {
+    return a + b;
   }
 }
