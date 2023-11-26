327b13cfce1e63ae473db30fe2fbef46489a651d
Tania Samoilenko <80690310+ania0005@users.noreply.github.com>
2023-11-26 12:57:56 +0100

    Update Main.java
    Homework completed.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
   public static void main(String[] args) {
     int x = 10;
     int y = 15;
-
+    
+    System.out.println(add(x, y));
   }
-}
+  public static int add(int x, int y){
+    returnx + y;
+  }
